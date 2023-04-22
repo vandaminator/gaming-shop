e969311f0c0ef3cc0e75574f2d82e1a2c531b8af
Phomolo Ntokoane <105977946+vandaminator@users.noreply.github.com>
2023-04-22 11:23:50 +0000

    similar function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import GameDb from "./utils/data/data";
 
 const gameDb = new GameDb
-const genres = gameDb.genres
-const tags = gameDb.tags
-console.log(gameDb.showFilter([genres[1]], [tags[3]]))
+// const genres = gameDb.genres
+// const tags = gameDb.tags
+// console.log(gameDb.showFilter([genres[1]], [tags[3]]))
+// console.log(gameDb.showSimilar('4200', 3))
 
 function App() {
   return <div>hello world</div>;
