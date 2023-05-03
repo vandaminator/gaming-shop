cb2b121b773947f81a9b3ad99eec575489389d18
Phomolo Ntokoane <105977946+vandaminator@users.noreply.github.com>
2023-05-03 12:28:38 +0000

    added drawer

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,38 +5,65 @@ import {
   InputGroup,
   Input,
   InputLeftElement,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
 } from "@chakra-ui/react";
 import { cancelIcon, logoIcon, menuIcon, searchIcon } from "../icons";
+import { useRef } from "react";
 
 function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const menuRef = useRef(document.getElementById("menu-btn"));
   return (
-    <Box
-      bg={"navyBlue"}
-      padding={"29px"}
-      color={"white"}
-      display={"flex"}
-      justifyContent={"space-between"}
-      alignItems={"center"}
-    >
-      <Button bg={"inherit"} w={"fit-content"} _hover={{bg: "inherit"}}>
-        <Image src={menuIcon} alt="" width={'40px'} />
-      </Button>
-      <a href="#">
-        <Image src={logoIcon} alt="" width={'60px'} />
-      </a>
-
-      <InputGroup
-        w={"40%"}
-        h={"fit-content"}
-        _focus={{ bg: "darkGrey" }}
-        bg={"darkGrey"}
-        borderRadius={"99"}
-        variant={"filled"}
+    <>
+      <Box
+        bg={"navyBlue"}
+        padding={"29px"}
+        color={"white"}
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
       >
-        <InputLeftElement children={<Image src={searchIcon} w={'20px'} />} />
-        <Input bg={"inherit"} borderRadius={"99"} placeholder="Search ..." />
-      </InputGroup>
-    </Box>
+        <Button
+          id="menu-btn"
+          bg={"inherit"}
+          w={"fit-content"}
+          _hover={{ bg: "inherit" }}
+          onClick={onOpen}
+        >
+          <Image src={menuIcon} alt="" width={"40px"} />
+        </Button>
+        <a href="#">
+          <Image src={logoIcon} alt="" width={"60px"} />
+        </a>
+
+        <InputGroup
+          w={"40%"}
+          h={"fit-content"}
+          _focus={{ bg: "darkGrey" }}
+          bg={"darkGrey"}
+          borderRadius={"99"}
+          variant={"filled"}
+        >
+          <InputLeftElement children={<Image src={searchIcon} w={"20px"} />} />
+          <Input bg={"inherit"} borderRadius={"99"} placeholder="Search ..." />
+        </InputGroup>
+      </Box>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={menuRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
 
