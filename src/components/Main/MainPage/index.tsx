d67866ad720e1653061519668a82c124c5adb631
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2023-05-16 14:25:12 +0200

    Update: Game Section done

--- src/components/Main/MainPage/index.tsx
+++ src/components/Main/MainPage/index.tsx
@@ -1,12 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import Games from "./Games";
 import TopGames2 from "./TopGames/TopGames2";
 
 function MainPage() {
   return (
-    <>
+    <Box> 
       <TopGames2 />
       <Games />
-    </>
+    </Box>
   );
 }
 
